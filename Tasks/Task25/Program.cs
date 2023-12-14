6d85483ccad4f138b8d56747b4c89ecf8ee41a13
Kseniia <ksysha_kasatkina@mail.ru>
2023-12-14 15:43:24 +0400

    edit

--- Tasks/Task25/Program.cs
+++ Tasks/Task25/Program.cs
@@ -3,5 +3,14 @@
 // Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем. 
 // Размер массива должен быть равен количеству цифр.
 
+int n = new Random().Next(1, 100001);
+Console.WriteLine(n);
+int[] array = new int[n.ToString().Length];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = n % 10;
+    n = n / 10;
+}
+Array.Reverse(array);
 
-
+Console.WriteLine(string.Join(" ", array));
